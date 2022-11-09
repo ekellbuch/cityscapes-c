a2dde04f8229b42aab1ea0127d0e8b8ce59a38c3
Kelly Buchanan <ekellbuchanan@gmail.com>
2022-11-09 10:12:03 -0500

    update dataset check

--- src/call_dataset.py
+++ src/call_dataset.py
@@ -16,10 +16,12 @@ from cityscapes_corrupted.cityscapes_corrupted import CityscapesCorrupted
 corruptions =  ["gaussian_noise","fog","brightness","contrast"]
 for corruption in corruptions:
   for severity in range(1, 6):
-    dataset_name=f'cityscapes_corrupted/semantic_segmentation_{}_{}'.format(corruption, severity)
+    dataset_name='cityscapes_corrupted/semantic_segmentation_{}_{}'.format(corruption, severity)
     builder = tfds.builder(dataset_name)
     builder.download_and_prepare()
-    builder.as_dataset()
+    builder.as_dataset(
+
+    )
     print(len(builder.as_dataset('validation').enumerate()))
 
 #%%

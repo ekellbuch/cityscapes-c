529fec9fa8611e3e08e64ee005b6df9d82ba7bb9
Kelly Buchanan <ekellbuchanan@gmail.com>
2022-11-08 22:10:11 -0500

    add tfds env variable to account for tfds issue 3903

--- src/call_dataset.py
+++ src/call_dataset.py
@@ -4,6 +4,13 @@ dataset = 'cityscapes'
 builder = tfds.builder(dataset)
 builder.as_dataset()
 #%%
+# set environment variable:
+# https://github.com/tensorflow/datasets/issues/3903
+import os
+os.environ['TFDS_DATA_DIR'] = os.environ.get('TFDS_DATA_DIR',
+                           os.path.join(os.environ.get('HOME'), 'tensorflow_datasets'))
+#%%
+from cityscapes_corrupted.cityscapes_corrupted import CityscapesCorrupted
 corruptions =  ["gaussian_noise","fog","brightness","contrast"]
 for corruption in corruptions:
   for severity in range(1, 6):
@@ -11,4 +18,6 @@ for corruption in corruptions:
     builder = tfds.builder(dataset_name)
     builder.download_and_prepare()
     builder.as_dataset()
+    print(len(builder.as_dataset('validation').enumerate()))
+
 #%%
